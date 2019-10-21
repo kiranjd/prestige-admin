38ef879b235d150a9b144fb3a46b9e4a0f098b6c
vrvembassy3 <vaishak.k@vrvembassy.com>
2019-10-21 16:55:59 +0530

    initial

--- src/app.jsx
+++ src/app.jsx
@@ -4,15 +4,22 @@ import Home from "./containers/home";
 import Shops from "./containers/shops";
 class App extends Component {
   state = {
-    location: "home"
+    location: "home",
+    name: ""
   };
   componentDidMount() {
     this.setState({
       location: window.location.href.split("/")[
         window.location.href.split("/").length - 1
-      ]
+      ],
+      name: "home"
     });
   }
+  getClass = name => {
+    this.setState({
+      name
+    });
+  };
   render() {
     return (
       <main className="app">
@@ -22,12 +29,60 @@ class App extends Component {
             <li>
               <img src="" alt="" />
             </li>
-            <li>Home</li>
-            <li className="active">Banners</li>
-            <li>Shops</li>
-            <li>Offers</li>
-            <li>Events</li>
-            <li>Help</li>
+            <li
+              name="home"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("home")}
+            >
+              Home
+            </li>
+            <li
+              name="banners"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("banners")}
+            >
+              Banners
+            </li>
+            <li
+              name="shops"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("shops")}
+            >
+              Shops
+            </li>
+            <li
+              name="offers"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("offers")}
+            >
+              Offers
+            </li>
+            <li
+              name="events"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("events")}
+            >
+              Events
+            </li>
+            <li
+              name="help"
+              className={
+                this.state.location === this.state.name ? "active" : ""
+              }
+              onClick={e => this.getClass("help")}
+            >
+              Help
+            </li>
           </ul>
         </aside>
         <div className="content">
