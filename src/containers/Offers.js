bb92c4b171743c2c45875e94bf5e7caa615ec646
vrvembassy3 <vaishak.k@vrvembassy.com>
2019-11-04 11:53:38 +0530

    tableheaders and modal forms added

--- src/containers/Offers.js
+++ src/containers/Offers.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import FormCardContainer from "../components/FormCardContainer";
-import Add from '@material-ui/icons/Add';
-import {  Grid, Typography } from "@material-ui/core";
+import Add from "@material-ui/icons/Add";
+import {
+  Input,
+  Grid,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 const shops = [
   {
@@ -19,87 +28,121 @@ const shops = [
     label: "shop 4"
   }
 ];
+
 function Offers() {
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleChange = () => {
+    console.log("b");
+  };
   const classes = useStyles();
-  const [shop, setShop] = React.useState("EUR");
+  const [isOneDayEvent, setIsOneDayEvent] = useState(false);
+  const [open, setOpen] = React.useState(false);
 
   // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   //   setShop(event.target.value);
   // };
   return (
-
-
-    <div style={{ padding: 20 }}>
-      <Grid container>
-        <Grid item xs={6} >
-          <Typography variant="h4">Offers</Typography>
+    <>
+      <div style={{ padding: 20 }}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h4">Offers</Typography>
+          </Grid>
+          <Grid item xs={6} style={{ textAlign: "right" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              startIcon={<Add />}
+              onClick={handleClickOpen}
+            >
+              Add Offers
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={6} style={{textAlign:"right"}}> 
-        <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        startIcon={<Add />}
+      </div>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
       >
-        Add Offers
-      </Button>
-        </Grid>
-      </Grid>
-    </div>
-    // <FormCardContainer>
-    //   <TextField
-    //     id="standard-basic"
-    //     className={classes.textField}
-    //     label="Name"
-    //     margin="normal"
-    //   />
+        <DialogContent>
+          <TextField
+            id="standard-basic"
+            className={classes.textField}
+            label="Name"
+            margin="normal"
+          />
 
-    //   <TextField
-    //     id="standard-textarea"
-    //     label="Description"
-    //     multiline
-    //     margin="normal"
-    //     className={classes.textField}
-    //   />
+          <TextField
+            id="standard-textarea"
+            label="Description"
+            multiline
+            margin="normal"
+            className={classes.textField}
+          />
 
-    //   <TextField
-    //     id="date"
-    //     type="date"
-    //     label="offer valid till"
-    //     placeholder="DD-MM-YYYY"
-    //     margin="normal"
-    //     className={classes.textField}
-    //     InputLabelProps={{
-    //       shrink: true
-    //     }}
-    //   />
+          <TextField
+            id="date"
+            type="date"
+            label="offer valid till"
+            placeholder="DD-MM-YYYY"
+            margin="normal"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
 
-    //   <TextField
-    //     id="Select Shop id"
-    //     select
-    //     label="Select-Shop"
-    //     className={classes.textField}
-    //     value={shop}
-    //     onChange={handleChange}
-    //     SelectProps={{
-    //       native: true,
-    //       MenuProps: {
-    //         className: classes.menu
-    //       }
-    //     }}
-    //     margin="normal"
-    //   >
-    //     {shops.map(option => (
-    //       <option key={option.value} value={option.value}>
-    //         {option.label}
-    //       </option>
-    //     ))}
-    //   </TextField>
-    //   <br />
-    //   <Button variant="contained" color="primary" className={classes.button}>
-    //     Login
-    //   </Button>
-    // </FormCardContainer>
+          <TextField
+            id="Select Shop id"
+            select
+            label="Select-Shop"
+            className={classes.textField}
+            // value={shop}
+            onChange={handleChange}
+            SelectProps={{
+              native: true,
+              MenuProps: {
+                className: classes.menu
+              }
+            }}
+            margin="normal"
+          >
+            {shops.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+          <br />
+          {/* <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Login
+          </Button> */}
+          {/* <Button variant="contained" color="primary" className={classes.button}>
+        Login
+      </Button> */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
--- src/containers/shops.jsx
+++ src/containers/shops.jsx
@@ -1,107 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-import Add from '@material-ui/icons/Add';
-import {  Grid, Typography } from "@material-ui/core";
+import Add from "@material-ui/icons/Add";
+import {
+  Input,
+  Grid,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  FormControlLabel,
+  Checkbox
+} from "@material-ui/core";
 
 import TextField from "@material-ui/core/TextField";
 import FormCardContainer from "../components/FormCardContainer";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
-import Input from "@material-ui/core/Input";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 
 function Shops() {
   const classes = useStyles();
-  const [floor, setFloor] = React.useState("");
-  const [category, setCategory] = React.useState("");
-  const handleChange = event => {
-    setFloor(event.target.value);
+  const [isOneDayEvent, setIsOneDayEvent] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
   return (
-
     <div style={{ padding: 20 }}>
       <Grid container>
-        <Grid item xs={6} >
+        <Grid item xs={6}>
           <Typography variant="h4">Shops</Typography>
         </Grid>
-        <Grid item xs={6} style={{textAlign:"right"}}> 
-        <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        startIcon={<Add />}
-      >
-        Add Shops
-      </Button>
+        <Grid item xs={6} style={{ textAlign: "right" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<Add />}
+            onClick={handleClickOpen}
+
+          >
+            Add Shops
+          </Button>
         </Grid>
       </Grid>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogContent>
+       <TextField
+        id="standard-basic"
+        className={classes.textField}
+        label="Shop Name"
+        margin="normal"
+      />
+      <TextField
+        id="standard-textarea"
+        label="Description"
+        multiline
+        margin="normal"
+        className={classes.textField}
+      />
+      <br />
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Floor</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          // onChange={handleChange}
+        >
+          <MenuItem value={0}>Ground</MenuItem>
+          <MenuItem value={1}>First</MenuItem>
+          <MenuItem value={2}>Second</MenuItem>
+          <MenuItem value={3}>Third</MenuItem>
+          <MenuItem value={4}>Fourth</MenuItem>
+        </Select>
+      </FormControl>
+      <br />
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Food</MenuItem>
+          <MenuItem value={20}>Entertainment </MenuItem>
+          <MenuItem value={30}>Electronics</MenuItem>
+          <MenuItem value={40}>Games</MenuItem>
+        </Select>
+      </FormControl>
+
+      <br />
+      <InputLabel className={classes.input}>
+        Upload Shop Logo
+        <Input
+          capture="camcorder"
+          className={classes.input}
+          id="icon-button-video"
+          type="file"
+        />
+      </InputLabel>
+
+      <br />
+      {/* <Button variant="contained" color="primary" className={classes.button}>
+        Login
+      </Button> */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
-    // <FormCardContainer>
-    //   <TextField
-    //     id="standard-basic"
-    //     className={classes.textField}
-    //     label="Shop Name"
-    //     margin="normal"
-    //   />
-    //   <TextField
-    //     id="standard-textarea"
-    //     label="Description"
-    //     multiline
-    //     margin="normal"
-    //     className={classes.textField}
-    //   />
-    //   <br />
-    //   <FormControl className={classes.formControl}>
-    //     <InputLabel id="demo-simple-select-label">Floor</InputLabel>
-    //     <Select
-    //       labelId="demo-simple-select-label"
-    //       id="demo-simple-select"
-    //       value={age}
-    //       onChange={handleChange}
-    //     >
-    //       <MenuItem value={0}>Ground</MenuItem>
-    //       <MenuItem value={1}>First</MenuItem>
-    //       <MenuItem value={2}>Second</MenuItem>
-    //       <MenuItem value={3}>Third</MenuItem>
-    //       <MenuItem value={4}>Fourth</MenuItem>
-    //     </Select>
-    //   </FormControl>
-    //   <br />
-    //   <FormControl className={classes.formControl}>
-    //     <InputLabel id="demo-simple-select-label">Category</InputLabel>
-    //     <Select
-    //       labelId="demo-simple-select-label"
-    //       id="demo-simple-select"
-    //       value={age}
-    //       onChange={handleChange}
-    //     >
-    //       <MenuItem value={10}>Food</MenuItem>
-    //       <MenuItem value={20}>Entertainment </MenuItem>
-    //       <MenuItem value={30}>Electronics</MenuItem>
-    //       <MenuItem value={40}>Games</MenuItem>
-    //     </Select>
-    //   </FormControl>
-
-    //   <br />
-    //   <InputLabel className={classes.input}>
-    //     Upload Shop Logo
-    //     <Input
-    //       capture="camcorder"
-    //       className={classes.input}
-    //       id="icon-button-video"
-    //       type="file"
-    //     />
-    //   </InputLabel>
-
-
-    //   <br />
-    //   <Button variant="contained" color="primary" className={classes.button}>
-    //     Login
-    //   </Button>
-    // </FormCardContainer>
+    
   );
 }
 
